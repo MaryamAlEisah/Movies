6185464b69fa128a2d635b001d09fb8e61c818d2
MaryamAlEisah <>
2021-04-14 13:02:23 +0000

    lab4

---
+++ app.js
@@ -0,0 +1,17 @@
+
+// alert
+
+alert("Hello");
+
+// prompt document.write
+
+var yourAge= prompt('please enter your yourAge');
+// console.log(yourAge);
+
+if(yourAge >= 18){
+  document.write('<h1>you are old</h1>');
+
+}else{document.write('<h1>you are age</h1>');
+}
+
+document.write("<img src='https://upload.wikimedia.org/wikipedia/en/2/2e/Nice_Time_%281957_film%29.jpg '>")
